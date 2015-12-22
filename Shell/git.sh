1a3666ee01c0f625071d866f9f29c72afecb07ef
fusion809 <brentonhorne77@gmail.com>
2015-12-23 08:31:58 +1000

    Updating git script

--- Shell/git.sh
+++ Shell/git.sh
@@ -69,11 +69,16 @@ function pushss {
 #############################################################
 # Sign in with SSH at startup
 # Makes contributing to GitHub projects a lot simpler.
-SSH_ENV=$HOME/.ssh/environment
-
-function ssh-setup {
-	ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
-}
+if [ -a $HOME/.ssh/environment ]
+then
+  SSH_ENV=$HOME/.ssh/environment
+elif [ $USER == fusion809 ]
+then
+  ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
+  SSH_ENV=$HOME/.ssh/environment
+  git config --global user.name "fusion809"
+  git config --global user.email "brentonhorne77@gmail.com"
+fi
 
 # start the ssh-agent
 # Remember, for this to work you need your SSH keys setup
