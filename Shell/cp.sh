0fc8011b6cd0e50eb57aa4ab9d31ee6f17e6227c
fusion809 <brentonhorne77@gmail.com>
2016-02-20 20:42:45 +0000

    Updating scripts

--- Shell/cp.sh
+++ Shell/cp.sh
@@ -1,4 +1,5 @@
 function cpsh {
   cp -a ~/GitHub/fedora-scripts/Shell/* ~/Shell/
   cp -a ~/GitHub/fedora-scripts/.bashrc ~/.bashrc
+  sudo cp -a ~/GitHub/fedora-scripts/root/{Shell,.bashrc} /root/
 }
