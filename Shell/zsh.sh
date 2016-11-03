e8a050510f7dded35d5a934a29baf486a447294f
fusion809 <brentonhorne77@gmail.com>
2016-11-03 17:54:39 +1000

    Adding zshf function

--- Shell/zsh.sh
+++ Shell/zsh.sh
@@ -5,3 +5,9 @@ function szsh {
 function nzsh {
 	nano $HOME/.zshrc
 }
+
+function zshf {
+	mv $HOME/.zsh_history $HOME/.zsh_history_bad
+	strings $HOME/.zsh_history_bad > $HOME/.zsh_history
+  fc -R $HOME/.zsh_history
+}
--- root/Shell/zsh.sh
+++ root/Shell/zsh.sh
@@ -13,3 +13,9 @@ function vzsh {
 function nzsh {
 	nano ~/.zshrc
 }
+
+function zshf {
+	mv $HOME/.zsh_history $HOME/.zsh_history_bad
+	strings $HOME/.zsh_history_bad > $HOME/.zsh_history
+  fc -R $HOME/.zsh_history
+}
