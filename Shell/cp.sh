ec614c8a675845eff0d99898093f88759f835525
Brenton Horne <brentonhorne77@gmail.com>
2016-12-08 06:01:46 +1000

    Rm cp.sh

--- Shell/cp.sh
+++
@@ -1,5 +0,0 @@
-function cpsh {
-  cp -a $HOME/GitHub/fedora-scripts/Shell/* $HOME/Shell/
-  cp -a $HOME/GitHub/fedora-scripts/.bashrc $HOME/.bashrc
-  sudo cp -a $HOME/GitHub/fedora-scripts/root/{Shell,.bashrc} /root/
-}
