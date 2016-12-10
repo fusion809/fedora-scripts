b4256fc3d435d9284981ec74dd98b08e4d2c88b1
Brenton Horne <brentonhorne77@gmail.com>
2016-12-11 04:52:23 +1000

    Updating scripts

--- Shell/programs/atom.sh
+++ Shell/programs/atom.sh
@@ -1,8 +1,17 @@
-ATOM_INSTALLED_VERSION=$(atom --version | cut -d ':' -f 2 | head -n 1 | sed 's/ //g')
-ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
+function atomup {
+  ATOM_INSTALLED_VERSION=$(atom --version | cut -d ':' -f 2 | head -n 1 | sed 's/ //g')
+  ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
 
-if [[ $ATOM_INSTALLED_VERSION < $ATOM_LATEST_VERSION ]]; then
-  sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
-elif [[ $ATOM_INSTALLED_VERSION > $ATOM_LATEST_VERSION ]]; then
-  printf "Something is wrong in $HOME/Shell/programs/atom.sh. The version $ATOM_LATEST_VERSION is being calculated to the latest stable Atom release version.\n"
+  if [[ $ATOM_INSTALLED_VERSION < $ATOM_LATEST_VERSION ]]; then
+    sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
+  elif [[ $ATOM_INSTALLED_VERSION > $ATOM_LATEST_VERSION ]]; then
+    printf "Something is wrong in $HOME/Shell/programs/atom.sh. The version $ATOM_LATEST_VERSION is being calculated to the latest stable Atom release version.\n"
+  fi
+}
+
+printf "" | zenity --progress --text "Display test" --auto-close 2> /dev/null
+if [[ $? -eq 0 ]]; then
+  atomup
+else
+  printf "X display is not available; likely running in a TTY"
 fi
