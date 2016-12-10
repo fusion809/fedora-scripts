c9f2082ecbaff620d4c5ffeebdc9fc4b0ebf63a5
Brenton Horne <brentonhorne77@gmail.com>
2016-12-11 00:36:49 +1000

    Updating scripts

--- Shell/02-variables.sh
+++ Shell/02-variables.sh
@@ -1,6 +1,7 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games
 export GHUB=$HOME/GitHub
 export GHUBM=$GHUB/mine
+export GHUBO=$GHUB/others
 export EDT=$GHUBM/editors
 export SCR=$GHUBM/scripts
 export PKG=$GHUBM/packaging
