4f3c3e911338009e66265ce0c173d815dc4f51f6
Brenton Horne <brentonhorne77@gmail.com>
2016-12-12 06:45:18 +1000

    Updating scripts

--- Shell/programs/trigger.sh
+++ Shell/programs/trigger.sh
@@ -1,3 +1,7 @@
 function trigger {
   bash <(curl -s https://raw.githubusercontent.com/fusion809/AppImages/master/trigger.sh) "$1"
 }
+
+function vimup {
+  bash <(curl -s https://raw.githubusercontent.com/fusion809/GVim.AppImage/master/trigger.sh)
+}
