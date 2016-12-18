df742962053cad1ec85c88c2acd577e87cd5e5fb
Brenton Horne <brentonhorne77@gmail.com>
2016-12-18 23:56:45 +1000

    Updating scripts

--- Shell/02-variables.sh
+++ Shell/02-variables.sh
@@ -1,12 +1,51 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games
 export GHUB=$HOME/GitHub
 export GHUBM=$GHUB/mine
+
+# GHUBO
 export GHUBO=$GHUB/others
+if ! [[ -d $GHUBO ]]; then
+  mkdir -p $GHUBO
+fi
+
+# EDT
 export EDT=$GHUBM/editors
+if ! [[ -d $EDT ]]; then
+  mkdir -p $EDT
+fi
+
+# SCR
 export SCR=$GHUBM/scripts
+if ! [[ -d $SCR ]]; then
+  mkdir -p $SCR
+fi
+
+# PKG
 export PKG=$GHUBM/packaging
+if ! [[ -d $PKG ]]; then
+  mkdir -p $PKG
+fi
+
+# WEB
 export WEB=$GHUBM/websites
+if ! [[ -d $WEB ]]; then
+  mkdir -p $WEB
+fi
+
+# ATO
 export ATO=$GHUBM/atom
+if ! [[ -d $ATO ]]; then
+  mkdir -p $ATO
+fi
+
+# FS
 export FS=$SCR/fedora-scripts
+if ! [[ -d $FS ]]; then
+  git clone https://github.com/fusion809/fedora-scripts $FS
+fi
+
+# FGI
 export FGI=$WEB/fusion809.github.io
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHUBO/dnf-utils/app/usr/lib/python3.5
+if ! [[ -d $FGI ]]; then
+  git clone https://github.com/fusion809/fusion809.github.io $FGI
+fi
--- Shell/programs/jekyll.sh
+++ Shell/programs/jekyll.sh
@@ -3,7 +3,7 @@ if ! `which jekyll > /dev/null 2>&1`; then
   if ! `which bundle exec jekyll > /dev/null 2>&1`; then
     if ! `which bundle > /dev/null 2>&1`; then
       if ! `which gem > /dev/null 2>&1`; then
-        sudo dnf install -y rubygems ruby-devel
+        sudo dnf install -y rubygems ruby-devel libxml2-static
       fi
       sudo gem install bundler
     fi
