c1f2548e1a1ab94b06370563b4b39c09eb1c7e45
Brenton Horne <brentonhorne77@gmail.com>
2016-12-18 23:50:04 +1000

    Updating scripts

---
+++ Shell/programs/jekyll.sh
@@ -0,0 +1,15 @@
+if ! `which jekyll > /dev/null 2>&1`; then
+  printf "Defining the jekyll command...\n"
+  if ! `which bundle exec jekyll > /dev/null 2>&1`; then
+    if ! `which bundle > /dev/null 2>&1`; then
+      if ! `which gem > /dev/null 2>&1`; then
+        sudo dnf install -y rubygems ruby-devel
+      fi
+      sudo gem install bundler
+    fi
+    pushd $FGI
+    bundle install
+    bundle update
+    popd
+  fi
+fi
