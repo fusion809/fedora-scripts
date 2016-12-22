59c694411f82b251971469fd33ba6ce5c572a70e
Brenton Horne <brentonhorne77@gmail.com>
2016-12-22 13:23:45 +1000

    Updating scripts

--- Shell/programs/vscode.sh
+++ Shell/programs/vscode.sh
@@ -6,7 +6,7 @@ fi
 VSCODE_URL=$(wget -cq http://code.visualstudio.com/updates -O - | grep "rpm-x64" | cut -d '"' -f 10)
 VSCODE_LATEST_VERSION=$(echo $VSCODE_URL | cut -d '/' -f 4)
 
-if [[ ${VSCODE_INSTALLED_VERSION//./} -neq ${VSCODE_LATEST_VERSION//./} ]]; then
+if (( ${VSCODE_INSTALLED_VERSION//./} < ${VSCODE_LATEST_VERSION//./} )); then
   wget -c $VSCODE_URL -O /tmp/code-${VSCODE_LATEST_VERSION}.x86_64.rpm
   sudo dnf install -y /tmp/code-${VSCODE_LATEST_VERSION}.x86_64.rpm
 fi
