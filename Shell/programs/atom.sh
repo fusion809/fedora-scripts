5a080cebdf509ea530c52153b827223912b79f25
Brenton Horne <brentonhorne77@gmail.com>
2016-12-22 13:22:06 +1000

    Updating scripts

--- Shell/programs/atom.sh
+++ Shell/programs/atom.sh
@@ -6,6 +6,6 @@ fi
 ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
 
 
-if (( ${ATOM_INSTALLED_VERSION//./} < ${ATOM_LATEST_VERSION//./} )); then
+if [[ ${ATOM_INSTALLED_VERSION//./} -neq ${ATOM_LATEST_VERSION//./} ]]; then
   sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
 fi
