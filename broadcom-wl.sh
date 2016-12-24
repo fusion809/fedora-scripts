b99cf55018c6acfac4e2e291505275d6a98a127d
Brenton Horne <brentonhorne77@gmail.com>
2016-12-24 15:39:53 +1000

    Updating

---
+++ broadcom-wl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+URL="https://download1.rpmfusion.org"
+FEDORA_VERSION=$(lsb_release -a | grep 'Release' | cut -d ':' -f 2 | sed 's/\s//g')
+
+# Install RPMFusion repos
+printf '\e[1;31m%-0s\e[m' "Installing RPMFusion repos for Fedora ${FEDORA_VERSION}...\n"
+sudo dnf install -y $URL/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_VERSION}.noarch.rpm $URL/free/fedora/rpmfusion-free-release-${FEDORA_VERSION}.noarch.rpm
+printf '\e[1;31m%-0s\e[m' "Updating all installed packages...\n"
+sudo dnf update -y
+printf '\e[1;31m%-0s\e[m' "Installing broadcom-wl...\n"
+sudo dnf install -y broadcom-wl
+printf '\e[1;31m%-0s\e[m' 'Probing the wl module...\n'
+sudo modprobe -a wl
