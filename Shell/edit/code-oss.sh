710bfaa5bf22a40470668e00789a720160b360b2
Brenton Horne <brentonhorne77@gmail.com>
2016-12-24 16:51:46 +1000

    Updating scripts

---
+++ Shell/edit/code-oss.sh
@@ -0,0 +1,13 @@
+function cblog {
+  code-oss $FGI/$1
+}
+
+alias cfgi=cblog
+
+function cosh {
+  code-oss $HOME/Shell/$1
+}
+
+function csp {
+  code-oss $HOME/Documents/CodeLite/CPP-Math-Projects/Simple-Pendulum/$1
+}
