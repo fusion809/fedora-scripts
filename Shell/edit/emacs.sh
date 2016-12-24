eaad6d7b89d530f02f7d027865494dd0f9f77df4
Brenton Horne <brentonhorne77@gmail.com>
2016-12-25 03:15:53 +1000

    Updating scripts

--- Shell/edit/emacs.sh
+++ Shell/edit/emacs.sh
@@ -1,16 +1,20 @@
 if ! [[ -f /usr/local/bin/emacs ]]; then
   FILENAME=$(curl -sL https://dl.bintray.com/fusion809/AppImages/ | grep "GNU_Emacs" | grep "\.AppImage<" | cut -d '"' -f 4 | cut -d ':' -f 2 | head -n 1)
   VERSION=$(echo $FILENAME | cut -d '-' -f 2)
-  printf "Downloading GNU Emacs $VERSION AppImage to $HOME"
-  if ! [[ -d $HOME/Programs/AppImages ]]; then
-    mkdir -p $HOME/Programs/AppImages
+  if ! [[ -f $HOME/Programs/AppImages/$FILENAME ]]; then
+    printf "Creating $HOME/Programs/AppImages directory...\n"
+    if ! [[ -d $HOME/Programs/AppImages ]]; then
+      mkdir -p $HOME/Programs/AppImages
+    fi
+    printf "Downloading GNU Emacs $VERSION AppImage to $HOME\n"
+    curl -L "https://dl.bintray.com/fusion809/AppImages/$FILENAME" > $HOME/Programs/AppImages/$FILENAME
   fi
-  curl -L "https://dl.bintray.com/fusion809/AppImages/$FILENAME" > $HOME/Programs/AppImages/$FILENAME
-  cat > /usr/local/bin/emacs <<\EOF
+  cat > emacs <<\EOF
   #!/bin/sh
   VERSION=$(ls /home/fusion809/Programs/AppImages | grep "Emacs" | cut -d "-" -f 2)
   "/home/fusion809/Programs/AppImages/GNU_Emacs-$VERSION-x86_64.AppImage" "$@"
 EOF
+sudo cp emacs /usr/local/bin
 cat > $HOME/.local/share/applications/appimagekit-emacs.desktop <<\EOF
 [Desktop Entry]
 Name=GNU Emacs
