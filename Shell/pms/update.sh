9e31c67e7dff81f6daab72c6e45aeb11edb8b99e
Brenton Horne <brentonhorne77@gmail.com>
2016-12-25 20:30:26 +1000

    Updating scripts

--- Shell/pms/update.sh
+++ Shell/pms/update.sh
@@ -1,7 +1,17 @@
 function update {
+	printf '\e[1;34m%-0s\e[m' "Running dnf update..."
+	printf "\n"
 	sduyr
+	printf '\e[1;34m%-0s\e[m' "Updating Flatpaks..."
+	printf "\n"
 	flatup
+	printf '\e[1;34m%-0s\e[m' "Updating Atom packages and themes..."
+	printf "\n"
 	apmup
+	printf '\e[1;34m%-0s\e[m' "Updating Arch Linux packages..."
+	printf "\n"
   archup
+	printf '\e[1;34m%-0s\e[m' "Updating Ubuntu packages..."
+	printf "\n"
 	ubuntuup
 }
