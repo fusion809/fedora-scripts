2aa85fa2e10f028f15acd2e821800347ebe286fb
Brenton Horne <brentonhorne77@gmail.com>
2017-01-03 23:01:59 +1000

    Updating scripts

--- Shell/pms/obs.sh
+++ Shell/pms/obs.sh
@@ -1,3 +1,8 @@
+if ! [[ -f /usr/bin/osc ]]; then
+  sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/openSUSE:Tools/Fedora_${FEDORA_VERSION}/openSUSE:Tools.repo
+  sudo dnf install -y osc obs-service-download_files obs-service-format_spec_file obs-service-source_validator
+fi
+
 function vimup {
   cdobsh vim
   pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
@@ -5,7 +10,6 @@ function vimup {
   patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
   vim_baseversion=$(cat vim.spec | grep "%define baseversion" | sed 's/%define baseversion\s*//g' | head -n 1)
   vim_patchversion=$(cat vim.spec | grep "%define patchlevel" | sed 's/%define patchlevel\s*//g' | head -n 1)
-
   if [[ $baseversion != $vim_baseversion ]]; then
     sed -i -e "s|$vim_baseversion|$baseversion|g" vim.spec
   fi
---
+++ Shell/variables/environment.sh
@@ -0,0 +1,1 @@
+export FEDORA_VERSION=$(cat /etc/fedora-release | cut -d " " -f 3)
