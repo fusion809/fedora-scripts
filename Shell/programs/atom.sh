456afccf759256102ba70b66ade745e89920fdfe
Brenton Horne <brentonhorne77@gmail.com>
2017-01-04 21:47:48 +1000

    Updating scripts

--- Shell/programs/atom.sh
+++ Shell/programs/atom.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
-if [[ -f /usr/bin/atom ]]; then
-  ATOM_INSTALLED_VERSION=$(rpm -qi atom | grep "Version" |  cut -d ':' -f 2 | cut -d ' ' -f 2)
-else
-  ATOM_INSTALLED_VERSION=""
-fi
-ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
+function atomup {
+  if [[ -f /usr/bin/atom ]]; then
+    ATOM_INSTALLED_VERSION=$(rpm -qi atom | grep "Version" |  cut -d ':' -f 2 | cut -d ' ' -f 2)
+  else
+    ATOM_INSTALLED_VERSION=""
+  fi
+  ATOM_LATEST_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f 4 | cut -d '/' -f 8 | sed 's/v//g')
 
-autoload is-at-least
+  autoload is-at-least
 
-if ! `is-at-least $ATOM_LATEST_VERSION ${ATOM_INSTALLED_VERSION:-0}`; then
-  sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
-fi
+  if ! `is-at-least $ATOM_LATEST_VERSION ${ATOM_INSTALLED_VERSION:-0}`; then
+    sudo dnf install -y https://github.com/atom/atom/releases/download/v${ATOM_LATEST_VERSION}/atom.x86_64.rpm
+  fi
+}
+
+#atomup
