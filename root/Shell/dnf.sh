f1f1ad66049287aee5476015f4b4ca526f9be25f
Brenton Horne <brentonhorne77@gmail.com>
2017-01-16 05:29:35 +1000

    Updating scripts

--- root/Shell/dnf.sh
+++ root/Shell/dnf.sh
@@ -2,9 +2,15 @@ function diy {
 	dnf install -y $@
 }
 
-function update {
+function duy {
 	dnf update -y
 }
+
+function update {
+	dnf update -y --refresh
+}
+
+alias duyr=update
 
 function ds {
 	dnf search "$@"
