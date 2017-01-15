6c9ed23e861eed6f9da3c0ce2bfb68f53f762448
Brenton Horne <brentonhorne77@gmail.com>
2017-01-16 03:57:40 +1000

    Updating scripts

--- Shell/pms/obs.sh
+++ Shell/pms/obs.sh
@@ -8,16 +8,16 @@ function ovimup {
   pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
   baseversion=$(echo $pkgver | sed 's/\.[0-9]*$//g')
   patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
-  vim_baseversion=$(cat vim.spec | grep "%define baseversion" | sed 's/%define baseversion\s*//g' | head -n 1)
-  vim_patchversion=$(cat vim.spec | grep "%define patchlevel" | sed 's/%define patchlevel\s*//g' | head -n 1)
+  vim_baseversion=$(cat vim.spec | grep "%define .*baseversion" | sed 's/%define .*baseversion\s*//g' | head -n 1)
+  vim_patchversion=$(cat vim.spec | grep "%define .*patchlevel" | sed 's/%define .*patchlevel\s*//g' | head -n 1)
   if [[ $baseversion != $vim_baseversion ]]; then
-    sed -i -e "s|$vim_baseversion|$baseversion|g" vim.spec
+    sed -i -e "s|baseversion $vim_baseversion|baseversion $baseversion|g" vim.spec
     if [[ -f PKGBUILD ]]; then
       sed -i -e "s|$vim_baseversion|$baseversion|g" PKGBUILD
     fi
   fi
   if [[ $patchversion != $vim_patchversion ]]; then
-    sed -i -e "s|$vim_patchversion|$patchversion|g" vim.spec
+    sed -i -e "s|patchlevel $vim_patchversion|patchlevel $patchversion|g" vim.spec
     if [[ -f PKGBUILD ]]; then
       sed -i -e "s|$vim_patchversion|$patchversion|g" PKGBUILD
     fi
