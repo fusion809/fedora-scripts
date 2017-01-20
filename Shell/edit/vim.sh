da94f4b66997102068edcc63227aef5da908a109
Brenton Horne <brentonhorne77@gmail.com>
2017-01-21 06:04:15 +1000

    Updating scripts

--- Shell/edit/vim.sh
+++ Shell/edit/vim.sh
@@ -1,13 +1,5 @@
-function vdk {
-  vim Dockerfile
-}
-
 function vfgi {
   vim $FGI
-}
-
-function vsp {
-  vim *.spec
 }
 
 for i in $HOME/Shell/edit/vim/*.sh
---
+++ Shell/edit/vim/local.sh
@@ -0,0 +1,7 @@
+function vdk {
+	vim Dockerfile
+}
+
+function vsp {
+	vim *.spec
+}
