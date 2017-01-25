810bcb27877dc39c96cacbe9016581990f07a323
Brenton Horne <brentonhorne77@gmail.com>
2017-01-25 14:22:00 +1000

    Updating scripts

---
+++ Shell/pms/archlinux.sh
@@ -0,0 +1,15 @@
+function pacrm {
+	sudo chroot /mnt /usr/bin/pacrm "$@"
+}
+
+function pacin {
+	sudo chroot /mnt /usr/bin/pacin "$@"
+}
+
+function pacain {
+	sudo chroot /mnt /usr/bin/pacain "$@"
+}
+
+function pacas {
+	sudo chroot /mnt /usr/bin/pacas "$@"
+}
