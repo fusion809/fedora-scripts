289df4ffc5910c95f21a75841aca486af439e8b4
Brenton Horne <brentonhorne77@gmail.com>
2017-01-25 12:12:41 +1000

    Updating scripts

--- Shell/variables/environment.sh
+++ Shell/variables/environment.sh
@@ -1,1 +1,3 @@
 export FEDORA_VERSION=$(cat /etc/fedora-release | cut -d " " -f 3)
+export CFE=$HOME/Documents/CodeLite/CPP-Math-Projects/Simple-Pendulum
+export CMP=$HOME/Documents/CodeLite/CPP-Math-Projects
