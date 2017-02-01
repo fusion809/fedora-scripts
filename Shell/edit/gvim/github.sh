dea07aec096f2c855a599fae9c67908200e0a5ec
Brenton Horne <brentonhorne77@gmail.com>
2017-02-01 22:22:39 +1000

    Updating scripts

--- Shell/edit/gvim/github.sh
+++ Shell/edit/gvim/github.sh
@@ -1,3 +1,19 @@
+# GitHub mine directory
+function gvgm {
+	pushd $GHUBM/$1
+	gvim
+	popd
+}
+
+# GitHub others directory
+function gvgo {
+	pushd $GHUBO/$1
+	gvim
+	popd
+}
+
+# fusion809.github.io repo
+# https://github.com/fusion809/fusion809.github.io
 function gvfgi {
 	pushd $FGI
 	gvim
@@ -6,36 +22,33 @@ function gvfgi {
 
 alias gvblog=gvfgi
 
-function gvgm {
-	pushd $GHUBM/$1
-	gvim
-	popd
-}
-
-function gvgo {
-	pushd $GHUBO/$1
-	gvim
-	popd
-}
-
+# Scripts
+# GNU_Octave repo
+# https://github.com/fusion809/GNU_Octave
 function gvoct {
 	pushd $SCR/GNU_Octave
 	gvim
 	popd
 }
 
+# JScripts repo
+# https://github.com/fusion809/JScripts
 function gvjs {
 	pushd $JS
 	gvim
 	popd
 }
 
+# python-scripts repo
+# https://github.com/fusion809/python-scripts
 function gvpy {
         pushd $PY
 	gvim
 	popd
 }
 
+# zsh-theme repo
+# https://github.com/fusion809/zsh-theme
 function gvzt {
 	pushd $SCR/zsh-theme
 	gvim
--- Shell/edit/vim/github.sh
+++ Shell/edit/vim/github.sh
@@ -1,3 +1,19 @@
+# GitHub mine directory
+function vgm {
+	pushd $GHUBM/$1
+	vim
+	popd
+}
+
+# GitHub others directory
+function vgo {
+	pushd $GHUBO/$1
+	vim
+	popd
+}
+
+# fusion809.github.io repo
+# https://github.com/fusion809/fusion809.github.io
 function vfgi {
 	pushd $FGI
 	vim
@@ -6,36 +22,33 @@ function vfgi {
 
 alias vblog=vfgi
 
-function vgm {
-	pushd $GHUBM/$1
-	vim
-	popd
-}
-
-function vgo {
-	pushd $GHUBO/$1
-	vim
-	popd
-}
-
+# Scripts
+# GNU_Octave repo
+# https://github.com/fusion809/GNU_Octave
 function voct {
 	pushd $SCR/GNU_Octave
 	vim
 	popd
 }
 
+# JScripts repo
+# https://github.com/fusion809/JScripts
 function vjs {
 	pushd $JS
 	vim
 	popd
 }
 
+# python-scripts repo
+# https://github.com/fusion809/python-scripts
 function vpy {
         pushd $PY
 	vim
 	popd
 }
 
+# zsh-theme repo
+# https://github.com/fusion809/zsh-theme
 function vzt {
 	pushd $SCR/zsh-theme
 	vim
