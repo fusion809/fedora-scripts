3bd52bb62a172eb0d466512d232919fb09d602af
Brenton Horne <fusion809@localhost.localdomain>
2017-11-11 14:30:41 +1000

    Updating

--- Shell/pms/obs.sh
+++ Shell/pms/obs.sh
@@ -1,8 +1,3 @@
-if ! [[ -f /usr/bin/osc ]]; then
-  sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/openSUSE:Tools/Fedora_${FEDORA_VERSION}/openSUSE:Tools.repo
-  sudo dnf install -y osc obs-service-download_files obs-service-format_spec_file obs-service-source_validator
-fi
-
 function ovimup {
   cdobsh $1
   pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
