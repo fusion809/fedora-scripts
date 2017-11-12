be27b3117c34431ff56075f31d293952c680ff77
Brenton Horne <brentonhorne77@gmail.com>
2017-11-13 01:34:34 +1000

    Adding Atom flatpak path to /bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games

--- Shell/variables/path.sh
+++ Shell/variables/path.sh
@@ -1,1 +1,1 @@
-export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games
+export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games:$HOME/.local/share/flatpak/app/io.atom.Atom/x86_64/stable/active/files/bin
