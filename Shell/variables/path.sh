c8a27fef0dd100ac5bb1837087e6605007de7f57
Brenton Horne <brentonhorne77@gmail.com>
2017-11-13 01:37:37 +1000

    Rm sec from PATH

--- Shell/variables/path.sh
+++ Shell/variables/path.sh
@@ -1,1 +1,2 @@
-export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games:$HOME/.local/share/flatpak/app/io.atom.Atom/x86_64/stable/active/files/bin
+export PATH=/bin:/sbin:/usr/bin:/usr/lib64/ccache:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/games
+
