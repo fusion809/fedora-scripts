ad19b5410e48715b9998f90340c49fb782ab7a8f
fusion809 <fusion809@users.noreply.github.com>
2017-12-19 20:27:35 +1000

    Updating

--- Shell/programs/chroot.sh
+++ Shell/programs/chroot.sh
@@ -31,3 +31,34 @@ function aroot {
     sudo chroot /arch /usr/local/bin/su-fusion809
 }
 
+function genroot {
+    if [[ -d $1/root/dev ]]; then
+         root="$1/root"
+    elif [[ -d $1/root00/dev ]]; then
+         root="$1/root00"
+    else
+         root="$1"
+    fi
+
+    if ! [[ -f "$root/proc/config.gz" ]]; then
+         sudo mount -t proc /proc "$root/proc"
+         sudo mount --rbind /dev "$root/dev"
+         sudo mount --make-rslave "$root/dev"
+         sudo mount --rbind /sys "$root/sys"
+         sudo mount --make-rslave "$root/sys"
+         sudo rm "$root/etc/resolv.conf"
+         sudo cp -L /etc/resolv.conf "$root/etc"
+    fi
+
+    if [[ -f $root/usr/local/bin/su-fusion809 ]]; then
+         sudo chroot "$root" /usr/local/bin/su-fusion809
+    elif [[ -f $root/bin/zsh ]]; then
+         sudo chroot "$root" /bin/zsh
+    else  
+         sudo chroot "$root" /bin/bash
+    fi
+
+    if [[ -f $root/usr/bin/dnf ]]; then
+         sudo touch "$root/.autorelabel"
+    fi
+}
