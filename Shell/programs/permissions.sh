d3160ad0615160415bbd34efb8cdea9b869b3c39
fusion809 <brentonhorne77@gmail.com>
2017-12-22 16:32:46 +1000

    Adding perms fixer

---
+++ Shell/programs/permissions.sh
@@ -0,0 +1,4 @@
+find /data -uid 0 -exec chown fusion809:wheel {} +
+find /data -uid 10 -exec chown fusion809:wheel {} +
+find /data -uid 993 -exec chown fusion809:wheel {} +
+find /data -uid 1050 -exec chown fusion809:wheel {} +
