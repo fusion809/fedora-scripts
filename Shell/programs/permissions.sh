765ace3dde22226df3c8bba7a4d431945523b580
fusion809 <brentonhorne77@gmail.com>
2017-12-22 17:32:48 +1000

    Adding permissions func

--- Shell/programs/permissions.sh
+++ Shell/programs/permissions.sh
@@ -1,4 +1,10 @@
-find /data -uid 0 -exec chown fusion809:wheel {} +
-find /data -uid 10 -exec chown fusion809:wheel {} +
-find /data -uid 993 -exec chown fusion809:wheel {} +
-find /data -uid 1050 -exec chown fusion809:wheel {} +
+function fix-perms-uid {
+    sudo find /data -uid 0 -exec chown fusion809:wheel {} +
+    sudo find /data -uid 10 -exec chown fusion809:wheel {} +
+    sudo find /data -uid 993 -exec chown fusion809:wheel {} +
+    sudo find /data -uid 1050 -exec chown fusion809:wheel {} +
+}
+
+function fix-perms {
+    sudo find /data -exec chown fusion809:wheel {} +
+}
