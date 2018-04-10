6f1ece738ead5cdb6b42e6c871399b2e060318ff
fusion809 <brentonhorne77@gmail.com>
2018-04-10 15:14:41 +1000

    Adding the NIX_SSL_CERTS var

--- Shell/variables/environment.sh
+++ Shell/variables/environment.sh
@@ -1,3 +1,4 @@
 export FEDORA_VERSION=$(cat /etc/fedora-release | cut -d " " -f 3)
 export CFE=$HOME/Documents/CodeLite/CPP-Math-Projects/Simple-Pendulum
 export CMP=$HOME/Documents/CodeLite/CPP-Math-Projects
+export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
